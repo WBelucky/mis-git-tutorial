4d3558ec000ea516c1af6764a237fab23fcbb737
Soeking <great.from.g@gmail.com>
2019-12-20 19:22:30 +0900

    it's me

---
+++ me/Main.jl
@@ -0,0 +1,5 @@
+list=[106,117,108,105,97,32,105,115,32,103,111,111,100]
+for i in list
+    print(Char(i))
+end
+println()
---
+++ me/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val list= listOf(107,111,116,108,105,110,32,105,115,32,103,111,100)
+    list.forEach {
+        print(it.toChar())
+    }
+    println()
+}
